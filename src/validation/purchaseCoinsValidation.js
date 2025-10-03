08346ffc7f6ff19a381d1f1b3b84f148d169540f
zohaib-softaims <zohaib@thesoftaims.com>
2025-10-03 17:12:54 +0500

    add validation no more than 500pf purchased

--- src/validation/purchaseCoinsValidation.js
+++ src/validation/purchaseCoinsValidation.js
@@ -3,12 +3,12 @@ import { z } from "zod";
 const coinAmountSchema = z
   .string({
     required_error: "Number of coins is required",
-    invalid_type_error: "Number of coins must be a string",
   })
   .min(1, "Number of coins is required")
-  .regex(/^\d+$/, "Number of coins is required")
+  .refine((val) => /^\d+$/.test(val), "Number of coins must be digits only")
   .transform((val) => parseInt(val))
-  .refine((val) => val > 0, "Number of coins must be greater than 0");
+  .refine((val) => val > 0, "Number of coins must be greater than 0")
+  .refine((val) => val <= 500, "You cannot purchase more than 500 PF coins");
 
 export const purchaseCoinsSchema = z.object({
   coinAmount: coinAmountSchema,
