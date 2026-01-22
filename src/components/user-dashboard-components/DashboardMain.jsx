06bbece4b91a5e12806a5c9fb97da04c21240e80
zohaib-softaims <zohaib@thesoftaims.com>
2026-01-22 14:11:53 +0500

    refactor:remove the app download section

--- src/components/user-dashboard-components/DashboardMain.jsx
+++ src/components/user-dashboard-components/DashboardMain.jsx
@@ -1,10 +1,8 @@
-import AppDownloadSection from "./AppDownloadSection";
 import ManageSubscriptionSection from "./ManageSubscriptionSection";
 
 const DashboardMain = () => {
   return (
     <div className="min-h-screen p-6 space-y-6">
-      <AppDownloadSection />
       <ManageSubscriptionSection />
     </div>
   );
